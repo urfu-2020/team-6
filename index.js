0ad13d6e23e89bbe85326f7ac85959a774b0c889
Ивановский Дмитрий Александрович <d.ivanovskii@kontur>
2021-04-08 19:46:08 +0500

    [new] initial commit with linters

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const index = express();
+const port = 3000;
+index.get('/', (request, response) => {
+  response.send('Hello from Express!');
+});
+index.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err);
+  }
+  console.log(`server is listening on ${port}`);
+});
