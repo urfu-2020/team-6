55ed36e0dd16948066be994692d47efa356c86ee
Ивановский Дмитрий Александрович <d.ivanovskii@kontur>
2021-04-11 23:44:42 +0500

    [fix] fixes env variable name

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 80;
-const environment = process.env.NODE_ENV || 'DEVELOPMENT';
+const environment = process.env.PROJECT_ENV || 'DEVELOPMENT';
 const viewsDir = path.join(__dirname, 'views');
 const staticBasePath = environment === 'PRODUCTION' ? 'https://projectkilogram.surge.sh/' : '';
 
