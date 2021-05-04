af40cd5a59c3db735f411534ae302f5d0cca5c36
Ивановский Дмитрий Александрович <d.ivanovskii@kontur>
2021-05-05 00:20:27 +0500

    [new] adds message sending with simple polling

--- client/src/Components/UserItemsList.js
+++ client/src/Components/UserItemsList.js
@@ -2,12 +2,35 @@ import React from "react";
 import PropTypes from 'prop-types'
 import UserItem from "./UserItem";
 import './UserItemsList.css'
+import axios from "axios";
 
-function UserItemsList({users}) {
+import {useHistory} from "react-router-dom";
+
+const userClickHandle = (loggedInUser, userOnClick, usersWithChats, history) => {
+    const data = {
+        users: [loggedInUser.id, userOnClick.id]
+    };
+    if (usersWithChats[userOnClick.id]) {
+        const chat = usersWithChats[userOnClick.id];
+        history.push(`chats/${chat.id}`)
+    } else {
+        axios.post('/api/v1/chats', data)
+            .then(result => {
+                const chat = result.data;
+                history.push(`chats/${chat.id}`)
+            });
+    }
+};
+
+function UserItemsList({user, users, usersWithChats}) {
+    const history = useHistory();
+
     return (
         <div className="users_block">
-            {users.map((user, i) => {
-                return (<UserItem key={user.userName} userName={user.userName} avatarUrl={user.avatarUrl} />)
+            {users.map((u, _) => {
+                return (<UserItem onClick={() => userClickHandle(user, u, usersWithChats, history)} key={u.userName}
+                                  userName={u.userName}
+                                  avatarUrl={u.avatarUrl}/>)
             })}
         </div>
     )
