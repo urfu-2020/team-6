04a5fe727c2a2234ce5ad84af12139bfdc863bc2
MidnightSun27 <persickshek@yandex.ru>
2021-05-17 17:07:35 +0500

    Web sockets + a little bit of front

--- client/src/Components/ChatPage.js
+++ client/src/Components/ChatPage.js
@@ -1,17 +1,66 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import './ChatPage.css'
 import MessageForm from "./MessageForm";
 import MessagesList from "./MessagesList";
+import Header from "./Header";
 import {useParams} from "react-router-dom";
+import axios from "axios";
+
 
 function ChatPage() {
     const {id} = useParams();
 
+    const [user, setUser] = React.useState(null);
+    const savedSocket = useRef();
+    const [messages, setMessages] = React.useState([]);
+
+
+    useEffect(async() => {
+        const result =  await axios.get(`/api/v1/chats/${id}/messages`);
+        const messages = result.data.messages;
+        setMessages(messages)
+    }, []);
+
+    if (user && !savedSocket.current) {
+        const userId = user.id;
+        savedSocket.current = new WebSocket(`ws://localhost:3001/${userId}`);
+        savedSocket.current.bufferType = "arraybuffer";
+        savedSocket.current.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+            setMessages(prevMessages => [...prevMessages, message]);
+        };
+    }
+
+    const sendMessage = (messageText, messageDate, chatId, userId) => {
+        const data = {
+            type: "sendMessage",
+            text: messageText,
+            date: messageDate,
+            chatId: chatId,
+            userId: userId
+        };
+        savedSocket.current.send(JSON.stringify(data));
+    };
+
+    React.useEffect(() => {
+        axios.get('/profile')
+            .then(result => {
+                const user = result.data;
+                setUser(user)
+            })
+            .catch(error => {
+                setUser(null);
+            });
+    }, []);
+
     return (
-        <div className="ChatPage">
-            <MessagesList chatId={id}/>
-            <MessageForm chatId={id}/>
-        </div>
+        user && (
+            <div className="ChatPage">
+                <Header/>
+                <MessagesList chatId={id} userId={user.id} messages={messages}/>
+                <MessageForm chatId={id} userId={user.id} sendMessage={sendMessage}/>
+            </div>
+        )
     );
 }
 
--- client/src/Components/MessagesList.js
+++ client/src/Components/MessagesList.js
@@ -1,49 +1,8 @@
 import React, {useEffect, useRef} from "react";
 import './MessagesList.css'
 import Message from "./Message";
-import axios from "axios";
 
-
-function useInterval(callback, delay) {
-    const savedCallback = useRef();
-
-    // Remember the latest callback.
-    useEffect(() => {
-        savedCallback.current = callback;
-    }, [callback]);
-
-    // Set up the interval.
-    useEffect(() => {
-        function tick() {
-            savedCallback.current();
-        }
-        if (delay !== null) {
-            let id = setInterval(tick, delay);
-            return () => clearInterval(id);
-        }
-    }, [delay]);
-}
-
-function MessagesList({chatId}) {
-    const [messages, setMessages] = React.useState([]);
-
-    useInterval(async () => {
-        const result =  await axios.get(`/api/v1/chats/${chatId}/messages`);
-        const messages = result.data.messages;
-        setMessages(messages)
-    }, 2000);
-
-    /*React.useEffect(() => {
-        axios.get(`/api/v1/chats/${chatId}/messages`)
-            .then(result => {
-                const messages = result.data.messages;
-                setMessages(messages)
-            })
-            .catch(error => {
-                setMessages([]);
-            });
-    }, [chatId]);*/
-
+function MessagesList({chatId, userId, messages}) {
     return (
         <div className='messages-list'>
             {messages.map((message) =>
